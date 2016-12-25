0e26394e1ec749398e74180f257f49a99096badd
Gardner von Holt <gardner@wvh.com>
2016-12-25 10:03:53 -0600

    Customization

--- CoreyGift/ViewController.swift
+++ CoreyGift/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var giveTo:String?
+    @IBOutlet weak var textGiveTo: UITextField!
+    @IBOutlet weak var labelGiveTo: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        if giveTo != nil {
+            labelGiveTo.text = giveTo
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +27,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        if segue.identifier != nil && segue.identifier == "DisplaySegue" {
+            (segue.destination as! ViewController).giveTo = textGiveTo.text
+        }
+    }
 }
 
